42ec396f74ad110617d6b33ae284f9e5adda8cce
Joshua Rich <joshua.rich@gmail.com>
2009-03-04 19:24:54 +1100

    Removed app-misc/yagtd, now in official Gentoo tree (thanks to Thilo Bangert).

--- app-misc/yagtd/files/50yagtd.el
+++
@@ -1,5 +0,0 @@
-;;; yagtd site-lisp configuration
-
-(setq load-path (cons "@SITELISP@" load-path))
-(autoload 'yagtd-mode "yagtd-mode" "yaGTD mode" t)
-(add-to-list 'auto-mode-alist '("\\.yagtd$" . yagtd-mode))
